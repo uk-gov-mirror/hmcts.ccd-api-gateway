51af2ce1a06c0a25eef285ff601275d1528f9f8f
Valentin Laurin <me@vlaurin.eu>
2018-10-31 17:50:16 +0000

    RDM-3271: Add palo alto
    Add palo alto with destination ip: ilbIp

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -95,3 +95,19 @@ module "api-gateway-web" {
     SECURE_AUTH_COOKIE_ENABLED = "true"
   }
 }
+
+module "palo_alto" {
+  source       = "git@github.com:hmcts/cnp-module-palo-alto.git"
+  subscription = "${var.subscription}"
+  env          = "${var.env}"
+  product      = "${var.product}"
+  common_tags  = "${var.common_tags}"
+
+  untrusted_vnet_name           = "core-infra-vnet-${var.env}"
+  untrusted_vnet_resource_group = "core-infra-${var.env}"
+  untrusted_vnet_subnet_name    = "palo-untrusted"
+  trusted_vnet_name             = "core-infra-vnet-${var.env}"
+  trusted_vnet_resource_group   = "core-infra-${var.env}"
+  trusted_vnet_subnet_name      = "palo-trusted"
+  trusted_destination_host      = "${var.ilbIp}"
+}
